8e9640f525f2700cade10815fdcad1292a0fc3d6
Necke <Necke@DESKTOP-4AIJTKI>
2023-12-27 13:42:35 +0700

    Программа разбита на отдельные файлы и добавлены тесты

---
+++ prak3/Valids.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct Valids {
+    const int year[2] = { 1800, 2024 };
+    const int month[2] = { 1, 12 };
+    const int day[2] = { 1 , 31 };
+    const int temperature[2] = { -60, 40 };
+    const vector<string> cities = { "Moskva", "Sankt-Peterburg",
+    "Novosibirsk", "Ekaterinburg", "Kazan", "Nijniy Novgorod",
+    "Chelyabinsk", "Samara", "Ufa", "Rostov-na-Donu", "Krasnodar",
+    "Omsk", "Voronej", "Perm", "Volgograd", "Krasnoyarsk"};
+};
